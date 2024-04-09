c9eb7584cd5dc66382f0f60c7eede8984823a909
kcay <ostaninmiha7@gmail.com>
2024-04-09 15:54:21 +0300

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,8 +5,9 @@ def main():
     print("[+] App started!")
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() 
-
+    client_socket, _ = server_socket.accept()
+    bytes = "HTTP/1.1 200 OK\r\n\r\n".encode("utf-8")
+    client_socket.send(bytes)
 
 if __name__ == "__main__":
     main()
